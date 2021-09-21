53609f403021b03c6ff125e348883877b3117b27
Kate A <ek.agapovaa@yandex.ru>
2021-09-21 20:56:56 +0300

    add PostManager

---
+++ src/main/java/manager/PostManager.java
@@ -0,0 +1,14 @@
+package manager;
+
+import domain.Post;
+
+public class PostManager {
+    private Post[] post;
+
+    public Post[] search(int ownerId, String domain, String query, boolean ownersOnly, int count, int offset) {
+        return null;
+    }
+
+    public void delete(int ownerId, int postId) {
+    }
+}
