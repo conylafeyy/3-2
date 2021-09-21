1cbd487a43cd95d326c60b99c8b61920f6af29aa
Kate A <ek.agapovaa@yandex.ru>
2021-09-21 20:21:05 +0300

    v1

---
+++ src/main/java/domain/Post.java
@@ -0,0 +1,130 @@
+package domain;
+
+public class Post {
+    private int id;
+    private int createdBy;
+    private int date;
+    private String text;
+    private LikesInfo likesInfo;
+    private CommentsInfo commentsInfo;
+    private RepostsInfo repostsInfo;
+    private ViewsInfo viewsInfo;
+    private String postType;
+    private GeoInfo geo;
+    private int signedId;
+    private int isPinned;
+    private int markedAsAds;
+    private boolean isFavorite;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getCreatedBy() {
+        return createdBy;
+    }
+
+    public void setCreatedBy(int createdBy) {
+        this.createdBy = createdBy;
+    }
+
+    public int getDate() {
+        return date;
+    }
+
+    public void setDate(int date) {
+        this.date = date;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public LikesInfo getLikesInfo() {
+        return likesInfo;
+    }
+
+    public void setLikesInfo(LikesInfo likesInfo) {
+        this.likesInfo = likesInfo;
+    }
+
+    public CommentsInfo getCommentsInfo() {
+        return commentsInfo;
+    }
+
+    public void setCommentsInfo(CommentsInfo commentsInfo) {
+        this.commentsInfo = commentsInfo;
+    }
+
+    public RepostsInfo getRepostsInfo() {
+        return repostsInfo;
+    }
+
+    public void setRepostsInfo(RepostsInfo repostsInfo) {
+        this.repostsInfo = repostsInfo;
+    }
+
+    public ViewsInfo getViewsInfo() {
+        return viewsInfo;
+    }
+
+    public void setViewsInfo(ViewsInfo viewsInfo) {
+        this.viewsInfo = viewsInfo;
+    }
+
+    public String getPostType() {
+        return postType;
+    }
+
+    public void setPostType(String postType) {
+        this.postType = postType;
+    }
+
+    public GeoInfo getGeo() {
+        return geo;
+    }
+
+    public void setGeo(GeoInfo geo) {
+        this.geo = geo;
+    }
+
+    public int getSignedId() {
+        return signedId;
+    }
+
+    public void setSignedId(int signedId) {
+        this.signedId = signedId;
+    }
+
+    public int getIsPinned() {
+        return isPinned;
+    }
+
+    public void setIsPinned(int isPinned) {
+        this.isPinned = isPinned;
+    }
+
+    public int getMarkedAsAds() {
+        return markedAsAds;
+    }
+
+    public void setMarkedAsAds(int markedAsAds) {
+        this.markedAsAds = markedAsAds;
+    }
+
+    public boolean isFavorite() {
+        return isFavorite;
+    }
+
+    public void setFavorite(boolean favorite) {
+        isFavorite = favorite;
+    }
+}
---
+++ src/main/java/domain/RepostsInfo.java
@@ -0,0 +1,22 @@
+package domain;
+
+public class RepostsInfo {
+    private int count;
+    private int user_reposted;
+
+    public int getCount() {
+        return count;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
+    }
+
+    public int getUser_reposted() {
+        return user_reposted;
+    }
+
+    public void setUser_reposted(int user_reposted) {
+        this.user_reposted = user_reposted;
+    }
+}
